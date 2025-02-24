7920076b16ecd65d68362589cbf96558b2186782
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-02-24 13:46:29 +0000

    Add pico and pico 2 benchmarks

--- bench_all.sh
+++ bench_all.sh
@@ -2,6 +2,17 @@
 
 pushd "$(dirname "$0")"
 
+read -p "Start with Pico 2 in bootsel mode"
+
+./gen_benchmarks.sh speedups 2.1.1
+./gen_benchmarks.sh 2.1.1
+./gen_benchmarks.sh 2.1.0
+./gen_benchmarks.sh 2.0.0
+
+read -p "Switch to Pico in bootsel mode"
+
+export PICO_BOARD="pico"
+
 ./gen_benchmarks.sh speedups 2.1.1
 ./gen_benchmarks.sh 2.1.1
 ./gen_benchmarks.sh 2.1.0
